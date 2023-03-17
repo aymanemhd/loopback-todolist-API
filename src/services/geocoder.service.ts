bd8213cfac7b5006f3dec75daf04c619b251ab55
aymanemhd <aymane.199820.mehdi@gmail.com>
2023-03-17 01:44:10 +0100

    generate geocode datasource and service

---
+++ src/services/geocoder.service.ts
@@ -0,0 +1,21 @@
+import {inject, Provider} from '@loopback/core';
+import {getService} from '@loopback/service-proxy';
+import {GeocoderDataSource} from '../datasources';
+
+export interface Geocoder {
+  // this is where you define the Node.js methods that will be
+  // mapped to REST/SOAP/gRPC operations as stated in the datasource
+  // json file.
+}
+
+export class GeocoderProvider implements Provider<Geocoder> {
+  constructor(
+    // geocoder must match the name property in the datasource json file
+    @inject('datasources.geocoder')
+    protected dataSource: GeocoderDataSource = new GeocoderDataSource(),
+  ) {}
+
+  value(): Promise<Geocoder> {
+    return getService(this.dataSource);
+  }
+}
